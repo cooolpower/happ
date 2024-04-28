9cc21e4808659ba3826452b3731f8f1a1f577cf8
James Kim <cooolpower@gmail.com>
2024-04-29 01:38:44 +0900

    chore: create library project

---
+++ index.js
@@ -0,0 +1,13 @@
+class Happ {
+  constructor(target) {
+    this.target = target;
+  }
+  modalOpen(target) {
+    console.log(target);
+    // const modalButton = document.querySelector(this.target);
+    // modalButton.addEventListner('click', {
+    //   modalButton.style.display = "block";
+    // });
+    // ;
+  }
+}
